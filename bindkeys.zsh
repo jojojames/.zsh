de52135b4d738b79b197de313b70435f054e29c5
James Nguyen <james@whoshere.net>
2016-03-31 13:57:20 -0700

    adding oh my zsh history completion to up/down

--- bindkeys.zsh
+++ bindkeys.zsh
@@ -14,4 +14,40 @@
 # bindkey "^B"      backward-char                        # ctrl-b
 # bindkey -v   # default to standard vi bindings, regardless of editor string
 
+###################################
+# oh-my-zsh
+###################################
+
+# Make sure that the terminal is in application mode when zle is active, since
+# only then values from $terminfo are valid
+if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
+    function zle-line-init() {
+        echoti smkx
+    }
+    function zle-line-finish() {
+        echoti rmkx
+    }
+    zle -N zle-line-init
+    zle -N zle-line-finish
+fi
+
+# start typing + [Up-Arrow] - fuzzy find history forward
+if [[ "${terminfo[kcuu1]}" != "" ]]; then
+    autoload -U up-line-or-beginning-search
+    zle -N up-line-or-beginning-search
+    bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
+fi
+# start typing + [Down-Arrow] - fuzzy find history backward
+if [[ "${terminfo[kcud1]}" != "" ]]; then
+    autoload -U down-line-or-beginning-search
+    zle -N down-line-or-beginning-search
+    bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
+fi
+
+bindkey ' ' magic-space                               # [Space] - do history expansion
+
+###################################
+# me
+###################################
 bindkey -e # use emacs bindings
+
