09e8e1e3a66e0d173c2b69b476cf4c3e81d1ac7a
James Nguyen <james@whoshere.net>
2016-04-08 17:58:57 -0700

    check against symlinks

--- setup.sh
+++ setup.sh
@@ -6,19 +6,19 @@ then
     touch $HOME/.z
 fi
 
-if [ -f $HOME/.zshrc ];
+if [ -L $HOME/.zshrc ];
 then
     echo "moving $HOME/.zshrc to $HOME/.zshrc_backup"
     mv $HOME/.zshrc $HOME/.zshrc_backup
 fi
 
-if [ -f $HOME/.zshenv ];
+if [ -L $HOME/.zshenv ];
 then
     echo "moving $HOME/.zshenv to $HOME/.zshenv_backup"
     mv $HOME/.zshenv $HOME/.zshenv_backup
 fi
 
-echo "linking $HOME/.zsh/zshrc to $HOME/.zshrc"
+echo "linking $HOME/.zsh/.zshrc to $HOME/.zshrc"
 ln -s $HOME/.zsh/.zshrc $HOME/.zshrc
-echo "linking $HOME/.zsh/zshenv to $HOME/.zshenv"
+echo "linking $HOME/.zsh/.zshenv to $HOME/.zshenv"
 ln -s $HOME/.zsh/.zshenv $HOME/.zshenv
