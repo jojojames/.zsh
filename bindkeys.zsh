acab1a44dca3b83c4e3862713a3e2349a4608a2d
James Nguyen <james@jojojames.com>
2017-08-18 00:13:31 -0700

    Fix trailing whitespace

--- bindkeys.zsh
+++ bindkeys.zsh
@@ -5,7 +5,7 @@
 # Examples
 # bindkey "^K"      kill-whole-line                      # ctrl-k
 # bindkey "^R"      history-incremental-search-backward  # ctrl-r
-# bindkey "^A"      beginning-of-line                    # ctrl-a  
+# bindkey "^A"      beginning-of-line                    # ctrl-a
 # bindkey "^E"      end-of-line                          # ctrl-e
 # bindkey "[B"      history-search-forward               # down arrow
 # bindkey "[A"      history-search-backward              # up arrow
--- setopts.zsh
+++ setopts.zsh
@@ -39,8 +39,8 @@ setopt hist_verify
 # imports new commands and appends typed commands to history
 #setopt share_history
 
-# ===== Completion 
-# When completing from the middle of a word, move the cursor to the end of the word    
+# ===== Completion
+# When completing from the middle of a word, move the cursor to the end of the word
 setopt always_to_end
 # show completion menu on successive tab press. needs unsetop menu_complete to work
 setopt auto_menu
