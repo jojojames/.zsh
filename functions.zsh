7e7e76836dfd111b0ead3d3dbc8824b8f3e7ba79
James Nguyen <james@jojojames.com>
2018-09-22 14:47:23 -0700

    Remove git aliases

--- functions.zsh
+++ functions.zsh
@@ -109,31 +109,6 @@ alias jformat='python -m json.tool'
 # remote machines
 # -------------------------------------------------------------------
 alias seedbox='ssh jojojames@genesis.whatbox.ca'
-# -------------------------------------------------------------------
-# Git
-# -------------------------------------------------------------------
-alias ga='git add'
-alias gp='git push'
-alias gl='git log'
-alias gpl="git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-alias gs='git status'
-alias gd='git diff'
-alias gm='git commit -m'
-alias gma='git commit -am'
-alias gb='git branch'
-alias gc='git checkout'
-alias gcb='git checkout -b'
-alias gra='git remote add'
-alias grr='git remote rm'
-alias gpu='git pull'
-alias gcl='git clone'
-alias gta='git tag -a -m'
-alias gf='git reflog'
-alias gv='git log --pretty=format:'%s' | cut -d " " -f 1 | sort | uniq -c | sort -nr'
-
-# leverage aliases from ~/.gitconfig
-alias gh='git hist'
-alias gt='git today'
 
 # curiosities
 # gsh shows the number of commits for the current repos for all developers
