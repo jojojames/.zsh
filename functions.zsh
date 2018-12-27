e991254ed9c448bc1d3e02882a5fa4c5bde2114d
James Nguyen <james@jojojames.com>
2018-12-27 10:22:36 -0800

    Remove unused functions

--- functions.zsh
+++ functions.zsh
@@ -33,12 +33,6 @@ if [[ $IS_MAC -eq 1 ]]; then
 fi
 
 # -------------------------------------------------------------------
-# nice mount (http://catonmat.net/blog/another-ten-one-liners-from-commandlingfu-explained)
-# displays mounted drive information in a nicely formatted manner
-# -------------------------------------------------------------------
-function nicemount() { (echo "DEVICE PATH TYPE FLAGS" && mount | awk '$2="";1') | column -t ; }
-
-# -------------------------------------------------------------------
 # myIP address
 # -------------------------------------------------------------------
 function myip() {
@@ -47,18 +41,6 @@ function myip() {
     ifconfig en0 | grep 'inet6 ' | sed -e 's/ / /' | awk '{print "en0 (IPv6): " $2 " " $3 " " $4 " " $5 " " $6}'
     ifconfig en1 | grep 'inet ' | sed -e 's/:/ /' | awk '{print "en1 (IPv4): " $2 " " $3 " " $4 " " $5 " " $6}'
     ifconfig en1 | grep 'inet6 ' | sed -e 's/ / /' | awk '{print "en1 (IPv6): " $2 " " $3 " " $4 " " $5 " " $6}'
-}
-
-# -------------------------------------------------------------------
-# console function
-# -------------------------------------------------------------------
-function console () {
-    if [[ $# > 0 ]]; then
-        query=$(echo "$*"|tr -s ' ' '|')
-        tail -f /var/log/system.log|grep -i --color=auto -E "$query"
-    else
-        tail -f /var/log/system.log
-    fi
 }
 
 # -------------------------------------------------------------------
