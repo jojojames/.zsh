a4628596c66534bd8e5f6e5c2caf5d842491951b
James Nguyen <james@jojojames.com>
2020-01-19 20:59:01 -0800

    Add fix for emacs + tramp + zsh

--- emacs.zsh
+++ emacs.zsh
@@ -28,3 +28,16 @@ dired() {
 }
 
 alias e=emacsclient2
+
+# https://www.reddit.com/r/emacs/comments/7f62pn/tramp_hang_after_sending_password/
+# Emacs tramp fix
+# https://wxchen.wordpress.com/2012/05/20/getting-tramp-in-emacs-to-work-with-zsh-as-default-shell/
+if [[ "$TERM" == "dumb" ]]
+then
+    unsetopt zle
+    unsetopt prompt_cr
+    unsetopt prompt_subst
+    unfunction precmd
+    unfunction preexec
+    PS1='$ '
+fi
