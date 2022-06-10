b92fb4d393deb8d6fa3af65f92d147f5c1af562b
James Nguyen <james@jojojames.com>
2022-06-10 20:39:37 +0000

    Disable LC_COLLATE and LC_ALL for work :O

--- exports.zsh
+++ exports.zsh
@@ -20,10 +20,13 @@ export ARCHFLAGS='-arch x86_64'
 export LESS='--ignore-case --raw-control-chars'
 export PAGER='less'
 
-# CTAGS Sorting in VIM/Emacs is better behaved with this in place
-export LC_COLLATE=C
-# https://stackoverflow.com/questions/11287564/getting-sed-error-illegal-byte-sequence-in-bash
-export LC_ALL=C
+if [[ $USER == "james" ]]; then
+  # These exports break on YT builds...
+  # CTAGS Sorting in VIM/Emacs is better behaved with this in place
+  export LC_COLLATE=C
+  # https://stackoverflow.com/questions/11287564/getting-sed-error-illegal-byte-sequence-in-bash
+  export LC_ALL=C
+fi
 
 # Node Version Manager
 # npm install -g n
